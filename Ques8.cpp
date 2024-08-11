d5faf397a796b371db686efba2e27c3133dc85ac
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-11 22:26:12 +0530

    Create Ques8.cpp

---
+++ Ques8.cpp
@@ -0,0 +1,21 @@
+/*You are given a three integers 'X', 'N', and 'M'. Your task is to find ('X' ^ 'N') % 'M'. A ^ B is defined as A raised to power B and A % C is the remainder when A is divided by C.*/
+
+#include <bits/stdc++.h>
+
+int modularExponentiation(int x, int n, int m)
+{
+	int res = 1;
+
+	while (n > 0)
+	{
+
+		if (n & 1)
+		{
+			// odd
+			res = (1LL * (res) * (x) % m) % m;
+		}
+		x = (1LL * (x) % m * (x) % m) % m;
+		n = n >> 1;
+	}
+	return res;
+}
