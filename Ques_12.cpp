3b35782aa0cf7a4588de3c10235ed6f21fc3ce1b
Shreyansh Katiyar <141402167+ShreyanshKatiyar007@users.noreply.github.com>
2024-08-18 16:59:36 +0530

    Create Ques_12.cpp

---
+++ Ques_12.cpp
@@ -0,0 +1,72 @@
+/*You have been given a sorted array/list 'arr' consisting of ‘n’ elements. You are also given an integer ‘k’.
+Now the array is rotated at some pivot point unknown to you.
+
+For example, if 'arr' = [ 1, 3, 5, 7, 8], then after rotating 'arr' at index 3, the array will be 'arr' = [7, 8, 1, 3, 5].
+
+Now, your task is to find the index at which ‘k’ is present in 'arr'.*/
+
+int getPivot(vector<int> &arr, int n)
+{
+
+    int s = 0;
+    int e = n - 1;
+    int mid = s + (e - s) / 2;
+
+    while (s < e)
+    {
+
+        if (arr[mid] >= arr[0])
+        {
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return s;
+}
+
+int binarySearch(vector<int> &arr, int s, int e, int key)
+{
+
+    int start = s;
+    int end = e;
+
+    int mid = start + (end - start) / 2;
+
+    while (start <= end)
+    {
+
+        if (arr[mid] == key)
+        {
+            return mid;
+        }
+
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+
+        mid = start + (end - start) / 2;
+    }
+    return -1;
+}
+
+int search(vector<int> &arr, int n, int k)
+{
+    int pivot = getPivot(arr, n);
+    if (k >= arr[pivot] && k <= arr[n - 1])
+    {
+        return binarySearch(arr, pivot, n - 1, k);
+    }
+    else
+    {
+        return binarySearch(arr, 0, pivot - 1, k);
+    }
+}
